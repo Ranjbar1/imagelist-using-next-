b16ca9f8ecba07e0f92a39f043c4e71152584fca
abolfazl <98ranjbar@gmail.com>
2022-05-01 11:00:18 +0430

    create New route for image browser app

---
+++ pages/ImageBrowser/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ImageList from "../../components/ImageList";
+import SearchBar from "../../components/SearchBar";
+
+const ImageBrowser = () => {
+  return (
+    <div>
+      <SearchBar />
+      <ImageList />
+    </div>
+  );
+};
+export default ImageBrowser;
