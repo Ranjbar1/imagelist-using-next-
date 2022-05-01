a3d429452ce57a6716db823923240495d7a32e77
abolfazl <98ranjbar@gmail.com>
2022-05-01 22:49:20 +0430

    Refactor image list component and modules

--- components/ImageList.jsx
+++ components/ImageList.jsx
@@ -1,6 +1,15 @@
 import React from "react";
-const ImageList = () => {
-  return <div>image list</div>;
+import { Image } from "next/image";
+const ImageList = ({ images }) => {
+  return (
+    <>
+      <div className="image-grid">
+        {images.map((image) => {
+          return <div key={image.id} className="item"></div>;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default ImageList;
