28e71db8b88673462d39f8783767e4231f9283bc
abolfazl <98ranjbar@gmail.com>
2022-05-01 11:01:18 +0430

    Fixed layout and components

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,7 +1,8 @@
+import styles from "../styles/Home.module.css";
 const Footer = () => {
   return (
-    <footer>
-      <div className="footer">
+    <footer className={styles.footer}>
+      <div>
         <h3>
           <p>lorem5</p>
         </h3>
