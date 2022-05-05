085447edbb7ea32d61eec8e4af33e17b05154c3c
abolfazl <98ranjbar@gmail.com>
2022-05-05 07:27:37 +0430

    Fixed image list

--- components/ImageList.jsx
+++ components/ImageList.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Image } from "next/image";
+import ImageCard from "./ImageCard";
 const ImageList = ({ images }) => {
   return (
     <>
       <div className="image-grid">
         {images.map((image) => {
-          return <div key={image.id} className="item"></div>;
+          return <ImageCard key={image.id}></ImageCard>;
         })}
       </div>
     </>
