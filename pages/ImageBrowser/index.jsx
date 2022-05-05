1451648d234c77016043ab383d9521e66f4e620e
abolfazl <98ranjbar@gmail.com>
2022-05-05 07:28:36 +0430

    ADDed Image browser rout

--- pages/ImageBrowser/index.jsx
+++ pages/ImageBrowser/index.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import ImageList from "../../components/ImageList";
 import SearchBar from "../../components/SearchBar";
+import unsplashAPI from "../api/unsplashAPI";
 
 const ImageBrowser = () => {
+  const [images, setImages] = useState([]);
+
+  let onSearchSubmition = async (term) => {
+    const response = await unsplashAPI.get("search/photos", {
+      params: { query: term },
+      headers: {},
+    });
+    images = response.data.results;
+    setImages(images);
+    console.log(images);
+  };
+
   return (
     <div>
-      <SearchBar />
-      <ImageList />
+      <SearchBar onSubmition={onSearchSubmition} />
+      <ImageList images={images} />
     </div>
   );
 };
