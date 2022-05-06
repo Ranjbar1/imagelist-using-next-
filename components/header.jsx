392dd353244955225c9d2b5b2d438982f0036af2
abolfazl <98ranjbar@gmail.com>
2022-05-06 18:07:16 +0430

    STYLE header style fixed

--- components/header.jsx
+++ components/header.jsx
@@ -1,5 +1,47 @@
 const Header = () => {
-  return <header className=""></header>;
+  return (
+    <header className="text-gray-900  sticky mx-0 my-3 font-semibold ">
+      <nav className=" flex flex-row justify-around flex-nowrap">
+        <ul className="flex space-x-2 py-1 ">
+          <li>
+            <span className="btn-header">Log In</span>
+          </li>
+          <li>
+            <span className="btn">Sign In</span>
+          </li>
+        </ul>
+        <ul className="flex flex-row justify-around flex-nowrap space-x-2 py-1">
+          <li>
+            <div className="item">
+              <span className="text-uppercase">Home</span>
+            </div>
+          </li>
+          <li>
+            <div className="item">
+              <span className="text-uppercase">Contact Me </span>
+            </div>
+          </li>
+          <li>
+            <div className="item">
+              <span className="text-uppercase">Video Browser</span>
+            </div>
+          </li>
+          <li>
+            <div className="item">
+              <span className="text-uppercase">Photo Browser</span>
+            </div>
+          </li>
+          <li>
+            <div className="item">
+              <span className="text-uppercase">
+                <i className="logo">logo</i>
+              </span>
+            </div>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
