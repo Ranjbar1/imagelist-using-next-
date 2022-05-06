1a4bbde24734e8044ed1200f4ea10de16e8bd8b3
abolfazl <98ranjbar@gmail.com>
2022-05-06 18:11:35 +0430

    STYLE search bar comp designed

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -9,19 +9,17 @@ const SearchBar = ({ onFormSubmit }) => {
   };
 
   return (
-    <div className="border-5 rounded ">
-      <form onSubmit={onSubmit} className="border-3 rounded ">
-        <div className="flex colomn ">
-          <label className="text-bold-3 border-3 rounded">
-            Search for Images !!
-          </label>
+    <div className="m-2 ml-4 sm:flex sm:justify-end sm:align-middle">
+      <form onSubmit={onSubmit} className="">
+        <div className="p-2">
+          <label className=" mr-3">Search for Images:</label>
           <input
             type="text"
-            className=""
+            className="border-2 border-blue-700 rounded focus:border-emerald-700 focus:border-2"
             value={term}
             onChange={(e) => setTerm(e.target.value)}
           />
-          <div className="text">You are looking for :{term}</div>
+          <div className="">You are looking for:{term}</div>
         </div>
       </form>
     </div>
