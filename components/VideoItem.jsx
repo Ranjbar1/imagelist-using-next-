ba0ea15898eaae1d72a923f7b63b55f6f6507105
abolfazl <98ranjbar@gmail.com>
2022-05-06 18:10:41 +0430

    STYLE video item comp designed

--- components/VideoItem.jsx
+++ components/VideoItem.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Image } from "next/image";
+import Image from "next/image";
 import VideoDetail from "./VideoDetail";
 
 const VideoItem = ({ video }) => {
   return (
-    <div className="">
+    <div className="bg-slate-100 rounded-md overflow-hidden shadow-md">
       <Image
+        className="p-1 ml-3 max-w-full"
+        height={25}
+        width={40}
         src={video.snippet.thumbnails.medium.url}
         alt={video.snippet.title}
       />
