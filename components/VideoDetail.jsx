74ee857a247656a955da1612a021e76ed5ac9e3c
abolfazl <98ranjbar@gmail.com>
2022-05-06 18:10:05 +0430

    STYLE  video detail comp styled

--- components/VideoDetail.jsx
+++ components/VideoDetail.jsx
@@ -2,11 +2,15 @@ import React from "react";
 const VideoDetail = ({ title, description, date }) => {
   return (
     <>
-      <div>
-        <div className="title">{title}</div>
-        <span>
-          <div>{description}</div>
-        </span>
+      <div className="my-1 h-10 w-40 px-2 text-left border-b-2 border-gray-400">
+        <div className="">
+          <span className="font-semibold overflow-clip ">{title}</span>
+        </div>
+        <div className="mt-1 overflow-hidden">
+          <span className="text-sm text-gray-700 opacity-75">
+            {description}
+          </span>
+        </div>
       </div>
     </>
   );
