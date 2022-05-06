b5a87db90d295f34f5429c2175b35faab3a06cb8
abolfazl <98ranjbar@gmail.com>
2022-05-06 21:37:58 +0430

    ADD about page and contact information

---
+++ pages/about.jsx
@@ -0,0 +1,83 @@
+import react from "react";
+import Image from "next/image";
+import Link from "next/link";
+const Contact = () => {
+  return (
+    <>
+      <div className="flex">
+        <div className="container">
+          <div className="img">
+            <Image src="/public/icons/icons8_gmail.ico" alt="gmail account " />
+          </div>
+          <div className="method-contact">
+            reach out to my via Gmail:
+            <Link
+              href="email:98ranjbar@gmail.com"
+              alt="my gmail at 98ranjbar@gmail.com"
+            >
+              Write down here!{" "}
+            </Link>
+          </div>
+        </div>
+        <div className="container">
+          <div className="img">
+            <Image src="/public/icons/icons8_gmail.ico" alt="gmail account " />
+          </div>
+          <div className="method-contact">
+            reach out to my via Gmail:
+            <Link
+              href="email:98ranjbar@gmail.com"
+              alt="my gmail at 98ranjbar@gmail.com"
+            >
+              Write down here!{" "}
+            </Link>
+          </div>
+        </div>
+        <div className="container">
+          <div className="img">
+            <Image src="/public/icons/icons8_gmail.ico" alt="gmail account " />
+          </div>
+          <div className="method-contact">
+            reach out to my via Gmail:
+            <Link
+              href="email:98ranjbar@gmail.com"
+              alt="my gmail at 98ranjbar@gmail.com"
+            >
+              Write down here!{" "}
+            </Link>
+          </div>
+        </div>
+        <div className="container">
+          <div className="img">
+            <Image src="/public/icons/icons8_gmail.ico" alt="gmail account " />
+          </div>
+          <div className="method-contact">
+            reach out to my via Gmail:
+            <Link
+              href="email:98ranjbar@gmail.com"
+              alt="my gmail at 98ranjbar@gmail.com"
+            >
+              Write down here!{" "}
+            </Link>
+          </div>
+        </div>
+        <div className="container">
+          <div className="img">
+            <Image src="/public/icons/icons8_gmail.ico" alt="gmail account " />
+          </div>
+          <div className="method-contact">
+            reach out to my via Gmail:
+            <Link
+              href="email:98ranjbar@gmail.com"
+              alt="my gmail at 98ranjbar@gmail.com"
+            >
+              Write down here!{" "}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
